a612f44db545e71d7495a3e2b11ea9e073a9d057
Ryan Pierpont <ryancpierpont@gmail.com>
2024-04-27 13:20:28 -0500

    config changes

--- web_project/settings.py
+++ web_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e037tcj3r0u98_1_p*42$b6#$6w+z2t&7l^cfhx9%uru3z-@b_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static_collected'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- web_project/urls.py
+++ web_project/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.urls import include, path
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", include("pierzada_app.urls")),
