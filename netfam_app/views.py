85e5e1f429ccddf096225a35e66e6ff2d122dd9b
Ryan Pierpont <ryancpierpont@gmail.com>
2024-04-27 11:45:22 -0500

    added template directory

--- netfam_app/views.py
+++ netfam_app/views.py
@@ -7,17 +7,12 @@ def home(request):
     return HttpResponse("Hello, Django!")
 
 def hello_there(request, name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
-
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return HttpResponse(content)
+    print(request.build_absolute_uri()) #optional
+    return render(
+        request,
+        'netfam_app/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
