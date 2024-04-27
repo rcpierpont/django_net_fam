ea414d46c077f49c79fadaa55a0b3e1c39da07a4
Ryan Pierpont <ryancpierpont@gmail.com>
2024-04-27 12:36:59 -0500

    gitignore should finally work this time

--- web_project/urls.py
+++ web_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", include("pierzada_app.urls")),
     path('admin/', admin.site.urls)
 ]
+urlpatterns += staticfiles_urlpatterns()
