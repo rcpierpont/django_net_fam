74d6ec91904b7453a3fb690abebf4f7085abf422
Ryan Pierpont <ryancpierpont@gmail.com>
2024-04-28 15:33:33 -0500

    basic site layout

--- pierzada_app/views.py
+++ pierzada_app/views.py
@@ -4,7 +4,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return render(request, "pierzada/home.html")
+
+def about(request):
+    return render(request, "pierzada/about.html")
+
+def contact(request):
+    return render(request, "pierzada/contact.html")
 
 #print("http://127.0.0.1:8000/hello/pierzada")
 def hello_there(request, name):
